1ad1b4bfe7dbde65c599c047630e0f83aa309d86
Chae JongWook <only9984@gmail.com>
2021-01-09 23:00:39 +0900

    [ADD]  - JPA 학습  - JPA 테스트 기능 학습

---
+++ src/main/java/com/cjw/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.cjw.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity // 클래스명의 카멜표기법에 따라 JPA에서 언더스코어 네이밍으로 사용
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ src/main/java/com/cjw/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.cjw.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+}
