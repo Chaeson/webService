f47bf144053456fca4867731fc7e97631609e538
Chae JongWook <only9984@gmail.com>
2021-01-12 23:30:37 +0900

    [ADD]  - 등록, 수정, 삭제 기능 구현 학습.  - 스프링시큐어리티 학습 예정.

--- src/main/java/com/cjw/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/cjw/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,14 @@
 package com.cjw.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT P FROM Posts P ORDER BY P.id DESC")
+    List<Posts> findAllDesc();
 
 }
